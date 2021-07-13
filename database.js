8156309a129f7ea3aaaaefdb7ffddada16d7ed81
Felipe Braz <fbraz.12@gmail.com>
2021-07-13 20:34:28 -0300

    Criação do database com os livros

---
+++ database.js
@@ -0,0 +1,76 @@
+const livros = [
+  {
+    id: 1,
+    nome: 'Introdução à Programação com Python',
+    autor: 'Nilo Ney Coutinho Menezes',
+    categoria: 'Programação',
+    paginas: 328,
+    recomenda: false,
+    leu: false
+  },
+  {
+    id: 2,
+    nome: 'Algoritmos E Lógica Da Programação',
+    autor: 'Marco A. Furlan de Souza',
+    categoria: 'Algoritmos',
+    paginas: 304,
+    recomenda: true,
+    leu: false
+  },
+  {
+    id: 3,
+    nome: 'Introdução à Linguagem SQL: Abordagem Prática',
+    autor: 'Thomas Nield',
+    categoria: 'Banco de dados',
+    paginas: 144,
+    recomenda: true,
+    leu: true
+  },
+  {
+    id: 4,
+    nome: 'R para data science',
+    autor: 'Hadley Wickham',
+    categoria: 'Programação',
+    paginas: 528,
+    recomenda: true,
+    leu: false
+  },
+  {
+    id: 5,
+    nome: 'Construindo Aplicações web com PHP e MySQL',
+    autor: 'André Milani',
+    categoria: 'Programação',
+    paginas: 336,
+    recomenda: true,
+    leu: true
+  },
+  {
+    id: 6,
+    nome: 'Fundamentos de HTML5 e CSS3',
+    autor: 'Maurício Samy Silva',
+    categoria: 'Web Design',
+    paginas: 304,
+    recomenda: true,
+    leu: true
+  },
+  {
+    id: 7,
+    nome: 'JavaScript - Guia do Programador',
+    autor: 'Maurício Samy Silva ',
+    categoria: 'Programação',
+    paginas: 608,
+    recomenda: true,
+    leu: false
+  },
+  {
+    id: 8,
+    nome: 'Javascript e Jquery: desenvolvimento de interfaces',
+    autor: 'Jon Duckett',
+    categoria: 'Programação',
+    paginas: 640,
+    recomenda: false,
+    leu: false
+  }
+]
+
+module.exports = livros
